3d46deb1243ec9ece61970d7912d7bf500ee826f
Justin Monge <justin.mnge@gmail.com>
2024-10-10 20:58:05 -0300

    fixed collisions issue with paddle

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import pygame
 from os.path import join
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1920, 1080
-SIZE = {'paddle': (40, 100), 'ball': (30, 30)}
+SIZE = {'paddle': (40, 125), 'ball': (30, 30)}
 POS = {'player': (WINDOW_WIDTH -50, WINDOW_HEIGHT / 2), 'opponent': (50, WINDOW_HEIGHT / 2)}
 SPEED = {'player': 500, 'opponent': 250, 'ball': 450}
 COLORS = {
